f9eaf89340105fb0dd722960b2b35f4388d9b2d5
yanghao89 <1491286207@qq.com>
2024-04-05 23:18:36 +0800

    init api routers

--- admin/internal/config/config.go
+++ admin/internal/config/config.go
@@ -9,4 +9,11 @@ type Config struct {
 	rest.RestConf
 	// mysql database config
 	MySQL xorm.DatabaseConf `json:"mysql" yaml:"mysql"`
+	Auth  Auth              `json:"auth"`
+}
+
+type Auth struct {
+	// jwt secret
+	JwtSecret string `json:"jwtSecret" default:"@G5U5*hY2e3E" yaml:"jwtSecret"`
+	Expire    int64  `json:"expire" default:"86400" yaml:"expire"`
 }
